027b11ff6ab1601f5b086727c52829a39608a390
Cédric <cedced19@gmail.com>
2015-04-26 16:33:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Gallery Creator
+
+Create a web gallery with some images.
+
+[![Build Status](https://travis-ci.org/cedced19/md-publisher.svg?branch=master)](https://travis-ci.org/cedced19/md-publisher)
+[![NPM version](https://badge.fury.io/js/md-publisher.svg)](http://badge.fury.io/js/md-publisher)
+
+```
+npm install -g gallery-creator
+```
+
+## Options
+
+```
+-h, --help                  output usage information
+
+-V, --version               output the version number
+
+-t  --title [string]         specified the title
+
+-c  --check         check if there are an update
+```
---
+++ gallery-creator.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+'use strict';
+var program = require('commander'),
+    colors = require('colors'),
+    pkg = require('./package.json'),
+    fs = require('fs');
+
+program
+    .version(pkg.version)
+    .option('-t, --title [string]', 'specified the title')
+    .option('-c, --check', 'check if there are an update')
+    .parse(process.argv);
+
+if (typeof program.title == 'undefined') {
+    console.log('Please set a title with -t.'.red);
+    program.title = 'Photos';
+}
+
+if (program.check) {
+    require('check-update')({packageName: 'check-update-tester', packageVersion: pkg.version, isCLI: true}, function(err, latestVersion, defaultMessage){
+        if(!err){
+            console.log(defaultMessage);
+        }
+    });
+}
+
+require('./lib/template')(program.title, function(content){
+    var time = Date.now();
+    fs.writeFile(process.cwd() + '/' + time + '.html', content, function (err) {
+        if (err) {
+            console.log('There was an error.'.red);
+            process.exit(1);
+        }
+        console.log(colors.green('Done! Generated in ' + time + '.html'));
+    });
+});
